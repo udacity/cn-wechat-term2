1e6825f37671b111100c991e6a906cad6a958366
evestorm <1572534835@qq.com>
2018-06-26 14:49:09 +0800

    Add login function.

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -1,4 +1,7 @@
 // pages/user/user.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+
 Page({
 
   /**
@@ -6,10 +9,6 @@ Page({
    */
   data: {
     userInfo: null,
-    // userInfo: {
-    //   nickName: "优达学城",
-    //   avatarUrl: "", // 头像 URL 地址
-    // }, // 虚拟数据
   },
 
   onTapAddress() {
@@ -30,55 +29,108 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+  },
+
+  onTapLogin: function () {
+    this.doQcloudLogin({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo
+        })
+      }
+    })
+  },
+
+  doQcloudLogin({ success, error }) {
+    // 调用 qcloud 登陆接口
+    qcloud.login({
+      success: result => {
+        if (result) {
+          let userInfo = result
+          success && success({
+            userInfo
+          })
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          this.getUserInfo({ success, error })
+        }
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
+
+  getUserInfo({ success, error }) {
+    qcloud.request({
+      url: config.service.requestUrl,
+      login: true,
+      success: result => {
+        let data = result.data
+
+        if (!data.code) {
+          let userInfo = data.data
+
+          success && success({
+            userInfo
+          })
+        } else {
+          error && error()
+        }
+      },
+      fail: () => {
+        error && error()
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
