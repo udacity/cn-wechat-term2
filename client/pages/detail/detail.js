9f900c34d358c14d3fa29509305f964b274301a5
evestorm <1572534835@qq.com>
2018-06-26 16:30:51 +0800

    Buy products from trolley page.

--- client/pages/detail/detail.js
+++ client/pages/detail/detail.js
@@ -58,7 +58,8 @@ Page({
       login: true,
       method: 'POST',
       data: {
-        list: [product]
+        list: [product],
+        isInstantBuy: true
       },
       success: result => {
         wx.hideLoading()
