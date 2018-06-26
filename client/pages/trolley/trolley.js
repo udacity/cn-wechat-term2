400207b745e4e06449f237d95f8cfc018579ce60
evestorm <1572534835@qq.com>
2018-06-26 15:50:30 +0800

    Pull trolley data from server.

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -11,22 +11,8 @@ Page({
   data: {
     userInfo: null,
     locationAuthType: app.data.locationAuthType,
-    trolleyList: [{
-      id: 1,
-      name: '商品1',
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
-      price: 45,
-      source: '海外·瑞典',
-      count: 1,
-    }, {
-      id: 2,
-      name: '商品2',
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
-      price: 158,
-      source: '海外·新西兰',
-      count: 3,
-    }], // 购物车商品列表
-    trolleyCheckMap: [undefined, true, undefined], // 购物车中选中的id哈希表
+    trolleyList: [], // 购物车商品列表
+    trolleyCheckMap: [], // 购物车中选中的id哈希表
     trolleyAccount: 45, // 购物车结算总价
     isTrolleyEdit: false, // 购物车是否处于编辑状态
     isTrolleyTotalCheck: true, // 购物车中商品是否全选
@@ -46,10 +32,47 @@ Page({
           userInfo,
           locationAuthType: app.data.locationAuthType
         })
+
+        this.getTrolley()
       },
       error: () => {
         this.setData({
           locationAuthType: app.data.locationAuthType
+        })
+      }
+    })
+  },
+
+  getTrolley() {
+    wx.showLoading({
+      title: '刷新购物车数据...',
+    })
+
+    qcloud.request({
+      url: config.service.trolleyList,
+      login: true,
+      success: result => {
+        wx.hideLoading()
+
+        let data = result.data
+
+        if (!data.code) {
+          this.setData({
+            trolleyList: data.data
+          })
+        } else {
+          wx.showToast({
+            icon: 'none',
+            title: '数据刷新失败',
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+
+        wx.showToast({
+          icon: 'none',
+          title: '数据刷新失败',
         })
       }
     })
@@ -75,6 +98,8 @@ Page({
         this.setData({
           userInfo
         })
+
+        this.getTrolley()
       }
     })
   },
--- server/routes/index.js
+++ server/routes/index.js
@@ -41,5 +41,7 @@ router.post('/order', validationMiddleware, controllers.order.add)
 router.get('/order', validationMiddleware, controllers.order.list)
 // 商品添加到购物车列表
 router.put('/trolley', validationMiddleware, controllers.trolley.add)
+// 获取购物车商品列表
+router.get('/trolley', validationMiddleware, controllers.trolley.list)
 
 module.exports = router
