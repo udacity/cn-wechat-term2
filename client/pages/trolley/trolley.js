77a191cb134f48797a427f84a5bd4e53ed2063c3
evestorm <1572534835@qq.com>
2018-06-26 15:33:59 +0800

    Add account bar on trolley page.

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -1,4 +1,6 @@
 // pages/trolley/trolley.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config')
 const app = getApp()
 
 Page({
@@ -8,7 +10,26 @@ Page({
    */
   data: {
     userInfo: null,
-    locationAuthType: app.data.locationAuthType
+    locationAuthType: app.data.locationAuthType,
+    trolleyList: [{
+      id: 1,
+      name: '商品1',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      price: 45,
+      source: '海外·瑞典',
+      count: 1,
+    }, {
+      id: 2,
+      name: '商品2',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      price: 158,
+      source: '海外·新西兰',
+      count: 3,
+    }], // 购物车商品列表
+    trolleyCheckMap: [undefined, true, undefined], // 购物车中选中的id哈希表
+    trolleyAccount: 45, // 购物车结算总价
+    isTrolleyEdit: false, // 购物车是否处于编辑状态
+    isTrolleyTotalCheck: true, // 购物车中商品是否全选
   },
 
   /**
