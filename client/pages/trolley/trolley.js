bb5743be8d3774c9bff55d016b88dc9415a1fa81
evestorm <1572534835@qq.com>
2018-06-26 16:00:31 +0800

    Add an edge case if total flag should be on/off when clicking a single product.

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -78,6 +78,51 @@ Page({
     })
   },
 
+  onTapCheckSingle(event) {
+    let checkId = event.currentTarget.dataset.id
+    let trolleyCheckMap = this.data.trolleyCheckMap
+    let trolleyList = this.data.trolleyList
+    let isTrolleyTotalCheck = this.data.isTrolleyTotalCheck
+    let numTotalProduct
+    let numCheckedProduct = 0
+
+    // 单项商品被选中/取消
+    trolleyCheckMap[checkId] = !trolleyCheckMap[checkId]
+
+    // 判断选中的商品个数是否需商品总数相等
+    numTotalProduct = trolleyList.length
+    trolleyCheckMap.forEach(checked => {
+      numCheckedProduct = checked ? numCheckedProduct + 1 : numCheckedProduct
+    })
+
+    isTrolleyTotalCheck = (numTotalProduct === numCheckedProduct) ? true : false
+
+    this.setData({
+      trolleyCheckMap,
+      isTrolleyTotalCheck
+    })
+  },
+
+  onTapCheckTotal(event) {
+    let trolleyCheckMap = this.data.trolleyCheckMap
+    let trolleyList = this.data.trolleyList
+    let isTrolleyTotalCheck = this.data.isTrolleyTotalCheck
+
+    // 全选按钮被选中/取消
+    isTrolleyTotalCheck = !isTrolleyTotalCheck
+
+    // 遍历并修改所有商品的状态
+    trolleyList.forEach(product => {
+      trolleyCheckMap[product.id] = isTrolleyTotalCheck
+    })
+
+    this.setData({
+      isTrolleyTotalCheck,
+      trolleyCheckMap
+    })
+
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
